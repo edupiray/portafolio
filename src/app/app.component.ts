6efc90c59a76fec2ff365f1c3372c06979289f77
Eduardo Piray <edupiray@gmail.com>
2021-08-10 13:16:50 -0300

    Listo para producción ajustes finales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ProyectoService } from './services/proyecto.service';
-import {NgxTypedJsModule} from 'ngx-typed-js';
 
 @Component({
   selector: 'app-root',
